5c79d2a5eed122f101d22915d59e672b7b6d9451
Julian <jdmesalosada@gmail.com>
2020-03-22 16:12:34 +0100

    added signup feature

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -1,10 +1,8 @@
-package java;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features"})
+@CucumberOptions(features = "src/test/resources/features")
 public class CucumberTest {
 }
